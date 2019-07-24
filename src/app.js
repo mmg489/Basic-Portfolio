6c523d8e3c76eac13b54b0ed62f34303e35a27f9
Margarita <m_mg489@outlook.com>
2019-07-23 23:59:00 -0400

    Lorem impsum

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const JumbotronPage = () => {
               <MDBCol className="py-5">
                 <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Create your beautiful website with MDBootstrap</MDBCardTitle>
                 <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                  optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
+                  optio ver. Odit sed qui, dolorum!
                 </p>
                 <MDBBtn outline color="yellow" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
               </MDBCol>
